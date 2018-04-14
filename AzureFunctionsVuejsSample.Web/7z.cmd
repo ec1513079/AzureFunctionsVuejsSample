533ccdf24d8cf6723dd110d605d74403b1468fd6
ec1513079 <ec1513079@gmail.com>
2018-04-14 16:33:45 +0900

    remove trash files

--- AzureFunctionsVuejsSample.Web/7z.cmd
+++
@@ -1,1 +0,0 @@
-@"%~dp0\node_modules\7zip\7zip-lite\7z.exe"   %*
--- AzureFunctionsVuejsSample.Web/geojson-rewind.cmd
+++
@@ -1,7 +0,0 @@
-@IF EXIST "%~dp0\node.exe" (
-  "%~dp0\node.exe"  "%~dp0\node_modules\geojson-rewind\geojson-rewind" %*
-) ELSE (
-  @SETLOCAL
-  @SET PATHEXT=%PATHEXT:;.JS;=;%
-  node  "%~dp0\node_modules\geojson-rewind\geojson-rewind" %*
-)
