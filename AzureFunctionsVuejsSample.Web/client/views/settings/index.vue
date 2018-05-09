2ac614abc03805a236ffa5a4abcacf99d8fd2460
ec1513079 <ec1513079@gmail.com>
2018-05-10 04:07:33 +0900

    impled 設定画面にAdminAPIの設定を追加

--- AzureFunctionsVuejsSample.Web/client/views/settings/index.vue
+++ AzureFunctionsVuejsSample.Web/client/views/settings/index.vue
@@ -3,15 +3,32 @@
     <div class="tile is-ancestor">
       <div class="tile is-parent">
         <article class="tile is-child box">
-          <h1 class="title">Azure Functions Settings</h1>
+          <h1 class="title">API Settings</h1>
           <div class="block">
             <p class="control">
-              <label class="label">Functions Endpoint</label>
-              <input class="input is-medium" type="text" :value="searchEndpoint" placeholder="Azure Functions Endpoint" disabled>
+              <label class="label">API Endpoint</label>
+              <input class="input is-medium" type="text" :value="apiEndpoint" placeholder="Azure Functions Endpoint" readonly>
             </p>
             <p class="control">
-              <label class="label">Functions API Key</label>
-              <input class="input is-medium" type="text" :value="searchKey" placeholder="Azure Functions API Key" disabled>
+              <label class="label">API Key (FOR DEBUB)</label>
+              <input class="input is-medium" type="text" :value="apiKey" placeholder="Azure Functions API Key" readonly>
+            </p>
+          </div>
+        </article>
+      </div>
+    </div>
+    <div class="tile is-ancestor">
+      <div class="tile is-parent">
+        <article class="tile is-child box">
+          <h1 class="title">Admin API Settings</h1>
+          <div class="block">
+            <p class="control">
+              <label class="label">API Endpoint</label>
+              <input class="input is-medium" type="text" :value="adminApiEndpoint" placeholder="Azure Functions Endpoint" readonly>
+            </p>
+            <p class="control">
+              <label class="label">API Key (FOR DEBUB)</label>
+              <input class="input is-medium" type="text" :value="adminApiKey" placeholder="Azure Functions API Key" readonly>
             </p>
           </div>
         </article>
@@ -23,38 +40,38 @@
           <h1 class="title">Account Settings</h1>
           <p class="control">
             <label class="label">Account Endpoint (FOR DEBUB)</label>
-            <input class="input is-medium" type="text" :value="authMeUrl" placeholder="AUTH Me Endpoint" disabled>
+            <input class="input is-medium" type="text" :value="authMeUrl" placeholder="AUTH Me Endpoint" readonly>
           </p>
           <p class="control">
             <label class="label">Account</label>
           </p>
           <p class="control has-icon">
-            <input class="input is-medium" type="text" v-model="name" placeholder="UserName" disabled>
+            <input class="input is-medium" type="text" v-model="name" placeholder="UserName" readonly>
             <span class="icon is-small">
               <i class="fa fa-user"></i>
             </span>
           </p>
           <p class="control has-icon">
-            <input class="input is-medium" type="email" v-model="email" placeholder="Email" disabled>
+            <input class="input is-medium" type="email" v-model="email" placeholder="Email" readonly>
             <span class="icon is-small">
               <i class="fa fa-envelope"></i>
             </span>
           </p>
           <p class="control">
             <label class="label">ID Token (FOR DEBUB)</label>
-            <input class="input is-medium" type="text" v-model="idToken" placeholder="ID Token" disabled>
+            <input class="input is-medium" type="text" v-model="idToken" placeholder="ID Token" readonly>
           </p>
           <p class="control">
             <label class="label">Access Token (FOR DEBUB)</label>
-            <input class="input is-medium" type="text" v-model="accessToken" placeholder="Access Token" disabled>
+            <input class="input is-medium" type="text" v-model="accessToken" placeholder="Access Token" readonly>
           </p>
           <p class="control">
             <label class="label">Admin Access Token (FOR DEBUB)</label>
-            <input class="input is-medium" type="text" v-model="adminToken" placeholder="Admin Access Token" disabled>
+            <input class="input is-medium" type="text" v-model="adminToken" placeholder="Admin Access Token" readonly>
           </p>
           <p class="control">
             <label class="label">Logout Endpoint (FOR DEBUB)</label>
-            <input class="input is-medium" type="text" :value="logoutUrl" placeholder="Logout Endpoint" disabled>
+            <input class="input is-medium" type="text" :value="logoutUrl" placeholder="Logout Endpoint" readonly>
           </p>
           <a class="button is-primary" :href="logoutUrl">
             <span class="icon">
@@ -73,9 +90,10 @@ export default {
 
   data () {
     return {
-      tableStorageConnectionStr: process.env.AZURE_STORAGE_CONNECTION_STRING,
-      searchEndpoint: process.env.AZURE_FUNCTIONS_ENDPOINT,
-      searchKey: process.env.AZURE_FUNCTIONS_API_KEY,
+      apiEndpoint: process.env.AZURE_FUNCTIONS_API_ENDPOINT,
+      apiKey: process.env.AZURE_FUNCTIONS_API_KEY,
+      adminApiEndpoint: process.env.AZURE_FUNCTIONS_ADMIN_API_ENDPOINT,
+      adminApiKey: process.env.AZURE_FUNCTIONS_ADMIN_API_KEY,
       authMeUrl: '/.auth/me',
       logoutUrl: '/.auth/logout?post_logout_redirect_uri=' + encodeURIComponent(process.env.LOGOUT_REDIRECT_URL)
     }
@@ -98,5 +116,9 @@ export default {
 <style lang="scss" scoped>
 .is-title {
   text-transform: capitalize;
+}
+
+.input[type='text'][readonly], .input[type='email'][readonly] {
+  background-color: whitesmoke
 }
 </style>
