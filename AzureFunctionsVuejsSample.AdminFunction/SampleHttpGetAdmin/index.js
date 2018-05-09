4fa8dac57e737aa73b2242c198542ab304a5b331
ec1513079 <ec1513079@gmail.com>
2018-05-10 03:49:40 +0900

    add AdminAPI用のAzureFunctionを追加

---
+++ AzureFunctionsVuejsSample.AdminFunction/SampleHttpGetAdmin/index.js
@@ -0,0 +1,15 @@
+const resMock = require('./mock.response.json')
+
+module.exports = function (context, req) {
+  context.log('"' + context.executionContext.functionName + '" is processed a request by ' + context.executionContext.invocationId)
+  context.res = {
+    status: 200,
+    headers: {
+      'Content-Type': 'application/json; charset=utf-8'
+    },
+    body: {
+      data: resMock
+    }
+  }
+  context.done()
+}
