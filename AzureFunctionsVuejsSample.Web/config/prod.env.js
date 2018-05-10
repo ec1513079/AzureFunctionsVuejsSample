5a0dbc934a5612e8322026736d5a56ccd712a09f
ec1513079 <ec1513079@gmail.com>
2018-05-10 14:02:51 +0900

    added 管理者用APIのAzure設定を追記

--- AzureFunctionsVuejsSample.Web/config/prod.env.js
+++ AzureFunctionsVuejsSample.Web/config/prod.env.js
@@ -5,8 +5,8 @@ module.exports = {
   AZURE_FUNCTIONS_API_ENDPOINT: '"https://ec-spa-ad-auth-func.azurewebsites.net/api"',
   AZURE_FUNCTIONS_API_KEY: '"eeCWaBmpwNIDq8MRsIiytHezhU0/CgaaD88VpbxML4sFhrjjRYYFaw=="',
   AZURE_FUNCTIONS_AD_APPLICATION_ID: '"fac0c5fb-9901-476f-a47a-4d9f6797f693"',
-  AZURE_FUNCTIONS_ADMIN_API_ENDPOINT: '""',
-  AZURE_FUNCTIONS_ADMIN_API_KEY: '""',
-  AZURE_FUNCTIONS_ADMIN_AD_APPLICATION_ID: '""',
-  LOGOUT_REDIRECT_URL: '""'
+  AZURE_FUNCTIONS_ADMIN_API_ENDPOINT: '"https://ec-spa-ad-auth-func-admin.azurewebsites.net/api"',
+  AZURE_FUNCTIONS_ADMIN_API_KEY: '"XerCqc0aG0lzefySsktVSmTzTcC6Hd3I86CCvy7ntpf8ovcf/IZe/g=="',
+  AZURE_FUNCTIONS_ADMIN_AD_APPLICATION_ID: '"8d7122e9-9492-4b4e-a077-a540c3173de3"',
+  LOGOUT_REDIRECT_URL: '"https://ec-spa-ad-auth-login.azurewebsites.net/"'
 }
